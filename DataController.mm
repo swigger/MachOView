21d1b59ec092e294b1134430c66ad75e4871f668
Vincent Tan <vincent.vtky@gmail.com>
2016-10-20 02:21:36 +0800

    crash cases

--- DataController.mm
+++ DataController.mm
@@ -163,7 +163,11 @@ NSString * const MVStatusTaskTerminated           = @"MVStatusTaskTerminated";
 //-----------------------------------------------------------------------------
 - (void)writeString:(NSString *)str toFile:(FILE *)pFile
 {
-  fwrite(CSTRING(str), [str length] + 1, 1, pFile);
+    if (!str) {
+        str = @"";
+    }
+    
+    fwrite(CSTRING(str), [str length] + 1, 1, pFile);
 }
 
 //-----------------------------------------------------------------------------
